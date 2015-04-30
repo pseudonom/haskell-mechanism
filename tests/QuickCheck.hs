b36bf2a1dea2c35dbae9aee18ba631396bbb100b
Eric Easley <escalier@riseup.net>
2015-04-30 09:03:43 -0400

    Testing scaffolding.

---
+++ tests/QuickCheck.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+import           Data.Functor ((<$>))
+
+import           Test.QuickCheck
+-- import           Distribution.TestSuite
+-- import           Distribution.TestSuite.QuickCheck   
+
+import           Mechanism
+import           Mechanism.Profile.Vector (Coll)
+import qualified Mechanism.Profile.Vector as P
+
+type ParetoInput a = (Coll (Coll a), Coll a)
+
+instance (Arbitrary a) => Arbitrary (Coll a) where
+  arbitrary = P.fromList <$> arbitrary
+
+instance (Arbitrary a) => Arbitrary (ParetoInput a) where
+  arbitrary = do
+    c <- arbitrary
+    cs <- fmap P.fromList . listOf $ P.fromList <$> vectorOf (P.length c) arbitrary
+    return (cs, c)
+
+pareto :: ParetoInput Int -> Bool
+pareto a = uncurry paretoOptimal a == uncurry paretoOptimal' a
+
+-- Fails with:
+-- Module ‘Distribution.Simple.Test’ does not export ‘stubMain’
+-- tests :: IO [Test]
+-- tests = return [testProperty "De Morgan's Pareto" pareto]
+
+main :: IO ()
+main = quickCheck pareto
